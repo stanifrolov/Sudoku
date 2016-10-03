20dbb5b03a63d9b756351a427fec4a230813b0a7
stanislavfrolov92 <mail.stanislav.frolov@gmail.com>
2016-10-03 14:35:30 +0200

    added box panels and gap between boxes

--- src/sudoku/SudokuGridLayout.java
+++ src/sudoku/SudokuGridLayout.java
@@ -1,6 +1,10 @@
 package sudoku;
 
+import java.awt.GridLayout;
+
 import javax.swing.JButton;
+import javax.swing.JPanel;
+
 
 public class SudokuGridLayout extends javax.swing.JFrame {
 	 
@@ -8,6 +12,7 @@ public class SudokuGridLayout extends javax.swing.JFrame {
 
 	private javax.swing.JPanel gamePanel = new javax.swing.JPanel();
 	private javax.swing.JPanel sudokuBoardPanel = new javax.swing.JPanel();
+	private javax.swing.JPanel[] sudokuBoxPanel = new javax.swing.JPanel[9];
 	private javax.swing.JPanel navigationPanel = new javax.swing.JPanel();
  
     private javax.swing.JButton[] button = new javax.swing.JButton[81];
@@ -29,23 +34,39 @@ public class SudokuGridLayout extends javax.swing.JFrame {
 	
 	public void setupPanels() {
 		gamePanel.setLayout(new java.awt.GridLayout(1, 2));
-
-        sudokuBoardPanel.setLayout(new java.awt.GridLayout(9, 9));
+		
+		GridLayout sudokuBoardLayout = new GridLayout(3, 3);
+		sudokuBoardLayout.setHgap(5);
+		sudokuBoardLayout.setVgap(5);
+        sudokuBoardPanel.setLayout(sudokuBoardLayout);
         gamePanel.add(sudokuBoardPanel);
-
+        
         navigationPanel.setLayout(new java.awt.GridLayout(5, 1));
         gamePanel.add(navigationPanel);
+        
+        for(int i = 0; i < 9; i++) {        	
+        	sudokuBoxPanel[i] = new JPanel();
+        	sudokuBoxPanel[i].setLayout(new java.awt.GridLayout(3, 3));
+        	sudokuBoardPanel.add(sudokuBoxPanel[i]);
+        }
         
         this.getContentPane().add(gamePanel);
 	}
 
-	public void setupSudokuBoard() {
+	public void setupSudokuBoard() {;
 		ButtonListener buttonListener = new ButtonListener();
+		int boxNumber;
         for(int i = 0; i < 81; i++) {
             button[i] = new javax.swing.JButton(Integer.toString(i + 1));
             button[i].addActionListener(buttonListener);
-            sudokuBoardPanel.add(button[i]);
-        }        
+            
+            boxNumber = getBoxNumberFromCell(i);
+            sudokuBoxPanel[boxNumber].add(button[i]);
+        }
+	}
+
+	public int getBoxNumberFromCell(int cellNumber) {
+		return cellNumber/9;
 	}
     
 	public void setupNavigation() {
